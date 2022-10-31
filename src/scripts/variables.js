e0f1f4f13be2c257736c6f00f1301d938909544c
Gustavo Rodrigues <Gustavorr001@gmail.com>
2022-10-31 11:01:20 -0300

    storing all application variables

---
+++ src/scripts/variables.js
@@ -0,0 +1,17 @@
+const search = document.querySelector('.search');
+const searchInput = document.querySelector('#searchInput');
+const city = document.querySelector('.city');
+const tempInfos = document.querySelector('.tempInfos span');
+const maxTemp = document.querySelector('.maxTemp span');
+const minTemp = document.querySelector('.minTemp span');
+const feelsLike = document.querySelector('.feels-like span')
+const humidity = document.querySelector('.humidity');
+const windSpeed = document.querySelector('.windSpeed');
+const atmosphericPressure = document.querySelector('.atmosphericPressure span');
+const cloudIcon = document.querySelector('.cloudIcon span');
+const windIcon = document.querySelector('.windIcon span');
+const currentDescription = document.querySelector('.currentWeatherDescription');
+const currentWeatherIcon = document.querySelector('.currentWeatherIcon');
+const result = document.querySelector('.results');
+
+export { search, searchInput, city, tempInfos, maxTemp, minTemp, feelsLike, humidity, windSpeed, atmosphericPressure, cloudIcon, windIcon, currentDescription, currentWeatherIcon, result }
